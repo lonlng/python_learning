c3acdfd744924b6dfdbd091892c0cd48cd552f61
kkl <lonlng@qq.com>
2019-10-31 17:30:00 +0800

    基本完成

--- 1031/Dictionary_.py
+++ 1031/Dictionary_.py
@@ -1,3 +1,5 @@
+import random
+
 topHitters = {"Gehrig": {"atBats": 8061, "hits": 2721}, "Ruth": {"atBats": 8399, "hits": 2873},
               "Willams": {"atBats": 7706, "hits": 2654}}
 print(topHitters['Gehrig']['hits'] / topHitters['Gehrig']['atBats'])
@@ -47,29 +49,64 @@ for i in splitString:
             splitString_2 = "".join(i.split(j, -1))
             i = splitString_2
     splitString_3.append(splitString_2)
-print("splitString_3",splitString_3)
-num=0
-num2 ={}
+print("splitString_3", splitString_3)
+num = 0
+num2 = {}
 for i in splitString_3:
-    num=0
+    num = 0
     for j in splitString_3:
-        if i == j :
-            num+=1
-    num3={i:num}
+        if i == j:
+            num += 1
+    num3 = {i: num}
     num2.update(num3)
 print(num2)
-# ifStr=''
-# for i in range(len(splitString)):
-#     if not splitString[i].isalpha():
-#         print(splitString[i])
-#         for splitWord in splitString[1]:
-#             if (65<=ord(splitWord) and 90>= ord(splitWord)) or (97<=ord(splitWord) and 122>= ord(splitWord)):
-#                 ifStr=ifStr+splitWord
-#         splitString[i]=ifStr
-#     ifStr=''
-print("函数")
-def circularArea(r):
-    Area = 3.14 * pow(r,2)
-    return Area
-print("圆的面积",circularArea(4))
 
+print("函数")
+
+
+def circularArea(r):
+    Area = 3.14 * pow(r, 2)
+    return Area
+
+
+print("圆的面积:", circularArea(4))
+
+print("三角形")
+
+def Triangle(a, b, c):
+    return (True) if (a + b) > c and (a + c) > b and (b + c) > a else (False)
+
+
+print("三角形判断", Triangle(33, 39, 30))
+
+print("list 十个整型数")
+
+
+def listNumber(x, y):
+    a = x[y:]
+    a = a + x[:y]
+    a.reverse()
+    return a
+
+
+aaa = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+bbb = listNumber(aaa, 6)
+print("listNumber(a,6)=", bbb)
+
+print("抽奖")
+a1 = 0
+a2 = 0
+a3 = 0
+for i in range(1000):
+    randomA = random.random()
+    if randomA > 0 and randomA < 0.08:
+        a1 += 1
+        continue
+    if randomA > 0.08 and randomA < 0.3:
+        a2 += 1
+        continue
+    if randomA > 0.2 and randomA < 1:
+        a3 += 1
+        continue
+ChouJiang = {'一等奖': a1, '二等奖': a2, '三等奖': a3}
+print(ChouJiang)
