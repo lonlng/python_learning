ac74cac6df917a3c006c047541893b30cb0d9ef6
kkl <lonlng@qq.com>
2019-10-31 15:47:28 +0800

    learn def

--- 1031/Dictionary_.py
+++ 1031/Dictionary_.py
@@ -29,5 +29,47 @@ for key3 in sinfoList:
 print(sinfo.keys())
 
 print(".5)")
+
 inputString = 'hello java, hello python, hello C.'
-print(inputString.split(' ', -1))
+
+splitString = inputString.split()
+print(splitString)
+print(type(splitString))
+
+splitString_3 = []
+print(type(splitString_3))
+for i in splitString:
+    splitString_2 = i
+    if not i.isalpha():
+        for j in (' .,*'):
+            if i.isalpha():
+                break
+            splitString_2 = "".join(i.split(j, -1))
+            i = splitString_2
+    splitString_3.append(splitString_2)
+print("splitString_3",splitString_3)
+num=0
+num2 ={}
+for i in splitString_3:
+    num=0
+    for j in splitString_3:
+        if i == j :
+            num+=1
+    num3={i:num}
+    num2.update(num3)
+print(num2)
+# ifStr=''
+# for i in range(len(splitString)):
+#     if not splitString[i].isalpha():
+#         print(splitString[i])
+#         for splitWord in splitString[1]:
+#             if (65<=ord(splitWord) and 90>= ord(splitWord)) or (97<=ord(splitWord) and 122>= ord(splitWord)):
+#                 ifStr=ifStr+splitWord
+#         splitString[i]=ifStr
+#     ifStr=''
+print("函数")
+def circularArea(r):
+    Area = 3.14 * pow(r,2)
+    return Area
+print("圆的面积",circularArea(4))
+
