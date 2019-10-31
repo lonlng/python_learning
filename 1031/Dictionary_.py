a7dc1283d2b6dc40a0ff9d689f6cfadb505c195c
kkl <lonlng@qq.com>
2019-10-31 20:15:21 +0800

    基本完成

--- 1031/Dictionary_.py
+++ 1031/Dictionary_.py
@@ -1,6 +1,6 @@
 import random
 
-print(".1)")
+print("\n.1)")
 # 赋值 topHitters 字典变量
 topHitters = {"Gehrig": {"atBats": 8061, "hits": 2721}, "Ruth": {"atBats": 8399, "hits": 2873},
               "Willams": {"atBats": 7706, "hits": 2654}}
@@ -9,21 +9,22 @@ topHitters = {"Gehrig": {"atBats": 8061, "hits": 2721}, "Ruth": {"atBats": 8399,
 for i in topHitters:
     print(i, '{:.2%}'.format(topHitters[i]['hits'] / topHitters[i]['atBats']))
 
-print('.2)')
+print('\n.2)')
 # 变量hits -- 总击中球次数, 将字典中元素值逐个相加
 hits = 0
 for i in topHitters:
     hits += topHitters[i]['hits']
 print(hits / 3)
 
-print(".3)max")
+print("\n.3)max")
 G = "Gehrig"
 G = "Ruth" if topHitters[G]['hits'] < topHitters['Ruth']['hits'] else "Gehrig"
-G = "Willams" if topHitters[G]['hits'] < topHitters['Willams']['hits'] else "Gehrig"
+G = "Willams" if topHitters[G]['hits'] < topHitters['Willams']['hits'] else "Ruth"
 print("击中球次数最多的一位:", topHitters[G]['hits'])
 
-print(".4)")
+print("\n.4)")
 sinfo = {"xiaohong": 0, "xiaolan": 0, "xiaoming": 1, "xiaobai": 1}
+print(sinfo)
 sinfoList = []
 # 将sinfo中的键赋值给sinfoList, 在下一for循环作为序列使用.
 for key in sinfo.keys():
@@ -31,9 +32,9 @@ for key in sinfo.keys():
 for key3 in sinfoList:
     if sinfo[key3] == 1:
         del sinfo[key3]  # 使用删除函数, 删除键
-print(sinfo.keys())
+print(sinfo)
 
-print(".5)")
+print("\n.5)")
 # 将inputString作为输入字符后变量
 # inputString = input()
 inputString = 'hello java, hello python, hello C.'
@@ -61,7 +62,7 @@ for i in splitStr:
     dictNum.update({i: num})
 print(dictNum)
 
-print("函数")
+print("\n函数")
 
 
 def circularArea(r):
@@ -69,7 +70,7 @@ def circularArea(r):
     return Area
 
 
-print("圆的面积:", circularArea(4))
+print("圆的面积:", circularArea(4), '\n')
 
 print("三角形")
 
@@ -78,7 +79,7 @@ def Triangle(a, b, c):
     return (True) if (a + b) > c and (a + c) > b and (b + c) > a else (False)
 
 
-print("三角形判断", Triangle(33, 39, 30))
+print("三角形判断", Triangle(33, 39, 30), '\n')
 
 print("list 十个整型数")
 
@@ -90,11 +91,11 @@ def listNumber(x, y):
     return a
 
 
-aaa = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-bbb = listNumber(aaa, 6)
-print("listNumber(a,6)=", bbb)
+Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# bbb = listNumber(aaa, 6)
+print(Number, '\n', listNumber(Number, 6))
 
-print("抽奖")
+print("\n抽奖")
 a1 = 0
 a2 = 0
 a3 = 0
@@ -110,4 +111,5 @@ for i in range(1000):
         a3 += 1
         continue
 ChouJiang = {'一等奖': a1, '二等奖': a2, '三等奖': a3}
-print(ChouJiang)
+for i in ChouJiang:
+    print(i, ':', ChouJiang[i])
