5a3164ffc7e3be00b861abef54c751ab25a1619b
Lenovo <lonlng@qq.com>
2019-10-31 11:38:35 +0800

    python_Max

--- topHitters_.py
+++ topHitters_.py
@@ -1,2 +1,13 @@
 topHitters={"Gehrig":{"atBats":8061,"hits":2721},"Ruth":{"atBats":8399,"hits":2873},"Willams":{"atBats":7706,"hits":2654}}
-print(topHitters['Gehrig'])
+print(topHitters['Gehrig']['hits']/topHitters['Gehrig']['atBats'])
+print(topHitters['Ruth']['hits']/topHitters['Ruth']['atBats'])
+print(topHitters['Willams']['hits']/topHitters['Willams']['atBats'])
+
+print((topHitters['Gehrig']['hits']+topHitters['Ruth']['hits']+topHitters['Willams']['hits'])/3)
+print("max")
+if topHitters['Gehrig']['hits']>topHitters['Ruth']['hits']and topHitters['Ruth']['hits'] >topHitters['Willams']['hits']:
+    print(topHitters['Gehrig']['hits'])
+elif topHitters['Ruth']['hits']>topHitters['Gehrig']['hits']and topHitters['Ruth']['hits']>topHitters['Willams']['hits']:
+    print(topHitters['Ruth']['hits'])
+else:
+    print(topHitters['Willams']['hits'])
