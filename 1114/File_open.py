9d81a9b1f87ec6308ddd491670250cb74aee96b1
kkl <lonlng@qq.com>
2019-11-16 15:24:44 +0800

    finish 08

--- 1114/File_open.py
+++ 1114/File_open.py
@@ -64,3 +64,57 @@ file_05_w.close()
 
 # 6)
 print("(6)")
+file_06_r = open('txt/Test_06.txt', 'r', encoding='UTF-8')
+file_06_a = open('txt/Test_06.txt', 'a', encoding='UTF-8')
+content_06 = file_06_r.read()
+file_06_r.close()
+userStr = 'alex'
+if userStr in content_06:
+    print("用户已存在")
+else:
+    file_06_a.write("\nalex")
+    print("用户已添加")
+
+# 7)
+print("(7)")
+file_07_r = open('txt/Test_07.txt', 'r', encoding='UTF-8')
+content_07 = file_07_r.readlines()
+file_07_r.close()
+idStr = '100003'
+for i in range(len(content_07)):
+    if idStr in content_07[i]:
+        del content_07[i]
+content_07 = ''.join(content_07)
+file_07_w = open('txt/Test_07_new.txt', 'w', encoding='UTF-8')
+file_07_w.write(content_07)
+file_07_w.close()
+print(content_07)
+
+# 8)
+print("(8)")
+inputStr = input()
+# inputStr = '5'
+while 1:
+    if inputStr.isdigit():
+        inputNum = int(inputStr)
+        if inputNum % 2 != 0:
+            break
+        else:
+            inputStr = input('请输入奇数')
+    else:
+        inputStr = input('请输入数字')
+order = list(range(inputNum))
+order2 = list(range(1, inputNum))
+order.reverse()
+order.extend(order2)
+stars = ''
+for i in order:
+    for S in range(i):
+        stars += ' '
+    for j in range((inputNum - i - 1) * 2 + 1):
+        stars += '*'
+    stars += '\n'
+print(stars)
+file_08_w = open('txt/Test_08.txt', 'w', encoding='UTF-8')
+file_08_w.write(stars)
+file_08_w.close()
